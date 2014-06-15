c893b3ad4d858382ed6dde575784c51d5f87aa8a
mnaylor <michnaylor@gmail.com>
2014-06-14 23:00:08 -0600

    Admin editing of catalog added

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.Linq;
+using System.Web.Mvc;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +8,21 @@ namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
